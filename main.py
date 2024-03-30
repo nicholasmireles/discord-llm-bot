3e612cce2b7808bb079a2c6b61f638326b50d978
nicholasmireles <nicholasjmireles@gmail.com>
2024-03-29 20:39:35 -0400

    major refactor

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import re
-import sys
-import requests
-import discord
-import yaml
+from typing import List
 
+import requests
+import yaml
+from discord import Client, Intents, Message
+
+INTENTS: Intents = Intents.default()
+INTENTS.message_content = True
 CONFIG: dict = {}
 
 with open("config.yaml") as f:
     CONFIG = yaml.load(f, Loader=yaml.FullLoader)
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-
-class MyClient(discord.Client):
+class ChatBotClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.listening_channel = None
+        self.current_conversation: List = []
 
-    async def message_history_to_context(self, message: discord.Message) -> dict:
+    async def message_to_context(self, message: Message) -> dict:
         transcript = "\n".join(
             [
-                f"{m.author.global_name if m.author.global_name else m.author.name}({m.author.name}): {re.sub(r"<.*>", "", m.content)}"
-                async for m in message.channel.history(limit=10)
-                if not m.author.bot
+                f"\"{m.author.global_name if m.author.global_name else m.author.name}\"({m.author.name}): {re.sub(r"<.*>", "", m.content)}"
+                for m in self.current_conversation
             ]
         )
-        context = f"""{CONFIG["cloudflare"]["context"]}
-        The following transcript will give you context for the conversation. The messages are in the format Name(username): message. Reply with just your message.
-        {transcript}
-        """
-
+        context = f"""{CONFIG["cloudflare"]["context"]} The following transcript will give you context for the conversation. The messages are in the format "Name"(username): message. Reply with just your message.\n{transcript}"""
         return {
             "messages": [
                 {"role": "system", "content": context},
@@ -38,7 +33,7 @@ class MyClient(discord.Client):
             ]
         }
 
-    async def query_model(self, message: discord.Message):
+    async def query_model(self, message: Message):
         """Query the LLM Model with the provided prompt.
 
         Args:
@@ -48,7 +43,7 @@ class MyClient(discord.Client):
             str: The model's response stripped of any metadata.
         """
 
-        prompt = await self.message_history_to_context(message)
+        prompt = await self.message_to_context(message)
 
         request = {
             "url": CONFIG["cloudflare"]["api_url"],
@@ -74,42 +69,43 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f"We have logged in as {self.user}")
 
-    async def on_message(self, message: discord.Message):
+    async def on_message(self, message: Message):
         try:
             match message:
-                case message if message.author == self.user:
+                case Message(author=self.user):
                     return
-                case message if self.user in message.mentions:
+                case Message(mentions=[self.user]):
                     message.content = re.sub(r"<@.*>", "@NickBot", message.content)
                     self.listening_channel = message.channel
                     await self.query_model(message)
-                case _:
-                    if self.listening_channel is not None:
-                        if message.content.startswith("$stop"):
-                            await self.listening_channel.send(
-                                "Shutting up :zipper_mouth:"
-                            )
-                            self.listening_channel = None
-                        else:
-                            await self.query_model(message)
+                case Message(channel=self.listening_channel) if self.listening_channel is not None:
+                    self.current_conversation.append(message)
+                    if message.content.startswith("$stop"):
+                        await self.listening_channel.send("Shutting up :zipper_mouth:")
+                        self.listening_channel = None
                     else:
-                        return
+                        await self.query_model(message)
+                case _:
+                    return
         except Exception as e:
             print(e)
             await message.channel.send(
-                "I've got a bug in my brain. Check my undies for details."
+                "I've got a bug in my brain."
             )
 
 
-test_request = {
-    "url": "https://api.cloudflare.com/client/v4/user/tokens/verify",
-    "headers": CONFIG["cloudflare"]["headers"],
-}
-response = requests.get(**test_request, timeout=30)
-if response.status_code != 200:
-    print(response.status_code)
-    print(response.text)
-    sys.exit()
 
-client = MyClient(intents=intents)
-client.run(CONFIG["discord"]["token"])
+def check_api_access():
+    test_request = {
+        "url": "https://api.cloudflare.com/client/v4/user/tokens/verify",
+        "headers": CONFIG["cloudflare"]["headers"],
+    }
+    response = requests.get(**test_request, timeout=30)
+    response.raise_for_status()
+
+
+if __name__ == "__main__":
+    if CONFIG["env"] == "dev":
+        check_api_access()
+    client = ChatBotClient(intents=INTENTS)
+    client.run(CONFIG["discord"]["token"])
